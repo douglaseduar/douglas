7dd78a800140055fe892fa37111709d774428947
douglaseduar <64625746+douglaseduar@users.noreply.github.com>
2021-03-01 20:21:52 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"/>
+  <link rel="stylesheet" href="style.css">
+  <link rel="shortcut icon" href="favi.png" type="image/x-icon">
+</head>
+<body>
+<a href="artes.html"><button class="circulo"><div class="descricao">Sites</div></button></a>
+  
+  <div class="profile-card">
+    <div class="card-header">
+      <div class="pic">
+        <img src="pic.jpg" alt="">
+      </div>
+      <div class="name">Douglas Eduardo</div>
+      <div class="desc">Developer & Designer</div>
+      <div class="sm">
+        <a href="https://www.facebook.com/douglas.eduardo.73997" class="fab fa-facebook-f"></a>
+        <a href="https://twitter.com/douglaseduar" class="fab fa-twitter"></a>
+        <a href="https://github.com/douglaseduar" class="fab fa-github"></a>
+        <a href="https://www.instagram.com/douglaseduar/" class="fab fa-instagram"></a>
+      </div>
+      <a href="http://localhost/dougrazzz/curriculo.pdf" target="_blank" class="contact-btn">README.txt</a>
+    </div>
+    <div class="card-footer">
+      <div class="numbers">
+        <div class="item">
+          <span>3</span>
+          Sites
+        </div>
+        <div class="border"></div>
+        <div class="item">
+          <span>+140</span>
+          Artes
+        </div>
+        <div class="border"></div>
+        <div class="item">
+          <span>4</span>
+          Softwares
+        </div>
+      </div>
+    </div>
+  </div>
+  <a href="https://www.behance.net/gallery/110224931/mounts"><button class="circulo"><div class="descricao">Artes</div></button></a>
+</body>
+</html>
